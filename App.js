edf059354d4b600290293dd66ff21104716e081c
root <ahmademir1201@gmail.com>
2018-10-27 19:12:12 +0700

    login

--- App.js
+++ App.js
@@ -7,55 +7,50 @@
  */
 import React, { Component } from 'react';
 import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
+import {  Title,  Footer, FooterTab, Button, Left, Right, Body, Icon} from 'native-base';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   TouchableOpacity,
   Linking,
+  View,
+  StatusBar
 } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
+import Routes from './src/Routes';
 
 
 export default class App extends Component<Props> {
-  onSuccess(e) {
-    Linking
-      .openURL(e.data)
-      .catch(err => alert(e.data));
-    //   .catch(err => console.error('An error occured', err));
+  // onSuccess(e) {
+  //   Linking
+  //     .openURL(e.data)
+  //     .catch(err => alert(e.data));
+  //   //   .catch(err => console.error('An error occured', err));
     
-  }
+  // }
 
   render() {
     return (
-      <QRCodeScanner
-        onRead={this.onSuccess.bind(this)}
-        topContent={
-          <Text style={styles.centerText}>
-            Welcome to <Text style={styles.textBold}>E-ticketing Emir</Text> scan your barcode in here.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
-      // <Container>
-      //   <Header />
-      //   <Content>
-      //     <Form>
-      //       <Item floatingLabel>
-      //         <Label>Username</Label>
-      //         <Input />
-      //       </Item>
-      //       <Item floatingLabel last>
-      //         <Label>Password</Label>
-      //         <Input />
-      //       </Item>
-      //     </Form>
-      //   </Content>
-      // </Container>
+      <View style={styles.container}>
+      <StatusBar backgroundColor="#1c313a" barStyle="light-content"/>
+      <Routes/>
+      </View>
+      // <QRCodeScanner
+      //   onRead={this.onSuccess.bind(this)}
+      //   topContent={
+      //     <Text style={styles.centerText}>
+      //       Welcome to <Text style={styles.textBold}>E-ticketing Emir</Text> scan your barcode in here.
+      //     </Text>
+      //   }
+      //   bottomContent={
+      //     <TouchableOpacity style={styles.buttonTouchable}>
+      //       <Text style={styles.buttonText}>OK. Got it!</Text>
+      //     </TouchableOpacity>
+      //   }
+      // />
+     
+      
 
     );
   }
@@ -79,4 +74,9 @@ const styles = StyleSheet.create({
   buttonTouchable: {
     padding: 16,
   },
+  container : {
+
+    flex: 1,
+    
+    }
 });
